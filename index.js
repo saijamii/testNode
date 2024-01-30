e02c4f92d5acf85d7305bf7aa0761fe52bd67173
saijamii <saijami38@gmail.com>
2024-01-30 08:21:44 +0530

    syntax changes

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 
-async function readNotes() {
+const readNotes = async () => {
   try {
     const data = await readFileAsync("notes.txt", "utf-8");
     console.log(data);
   } catch (error) {
     console.error(`Internal Server Error: ${error.message}`);
   }
-}
+};
 
 readNotes();
