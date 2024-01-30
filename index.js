cba6c8a47b41f24457b4011e942670fbf9d96624
saijamii <saijami38@gmail.com>
2024-01-30 08:30:32 +0530

    writeFileAsync

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
+const { writeFile } = require("fs/promises");
 const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
+const writeFileSync = util.promisify(fs.writeFile);
 
 const readNotes = async () => {
   try {
@@ -11,4 +13,17 @@ const readNotes = async () => {
   }
 };
 
-readNotes();
+const createNotes = async (fileName, data) => {
+  try {
+    await writeFileSync(fileName, data);
+    console.log(`File '${fileName}' created successfully.`);
+  } catch (error) {
+    res.status(500).json({
+      error: `Internal Server Error: ${error.message}`,
+    });
+  }
+};
+
+// readNotes();
+
+// createNotes("note1.txt", "This is the content of the new file.");
