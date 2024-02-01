0e7d166a299fcbe7e90f4b574cc7cfec70a129db
saijamii <saijami38@gmail.com>
2024-02-01 08:04:33 +0530

    http module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http");
 const { writeFile } = require("fs/promises");
 const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
@@ -27,3 +28,12 @@ const createNotes = async (fileName, data) => {
 // readNotes();
 
 // createNotes("note1.txt", "This is the content of the new file.");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Index.js");
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
