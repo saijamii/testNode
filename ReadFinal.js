e2e2a28c7a6be907e6e951f18e6f9afdbfbae429
saijami <saijami@unosimple.com>
2024-02-23 11:50:23 +0530

    Final Data Read file added

---
+++ ReadFinal.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`Data file ${PORT}`);
+});
+
+const readFinalData = async () => {
+  try {
+    const response = await readFileAsync("./FROM26.json", "utf-8");
+    const data = JSON.parse(response)?.map((e) => e);
+    console.log(data?.length);
+  } catch (error) {
+    console.error(error);
+  }
+};
+readFinalData();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
