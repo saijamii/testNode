358886ceed1ba686b03f0133bcd774f5a81206e3
saijami <saijami@unosimple.com>
2024-03-18 11:54:34 +0530

    csv file creation

--- Read.js
+++ Read.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const { parse } = require("csv-parse");
+const { pipeline } = require("stream");
+const csvWriter = require("csv-write-stream");
+const stringify = require("csv-stringify");
 
 const app = express();
 
@@ -12,25 +15,61 @@ app.get("/", (req, res) => {
 });
 
 const bigdataFile = () => {
-  const bigdata = [];
-  const daily = [];
-  fs.createReadStream("./CSV/Daily22.csv")
+  console.log("1");
+  let daily = new Set();
+  let bigdata = [];
+  let vendors = [];
+
+  fs.createReadStream("./CSV/newFile.csv")
     .pipe(parse())
     .on("data", (row) => {
-      row[0] !== "" && daily.push("531" + " | " + row[0]);
-      row[1] !== "" && bigdata.push("531" + " | " + row[1]);
+      if (row[0] !== "") {
+        daily.add(row[0]);
+      }
+      if (row[1] !== "") {
+        bigdata.push(row[1]);
+      }
+      if (row[2] !== "") {
+        vendors.push(row[2] + " | " + row[1]);
+      }
     })
     .on("end", async () => {
-      console.log(daily.length, bigdata.length);
-      let intersection = bigdata.filter((x) => !daily.includes(x));
-      let final = intersection.map((e) => [e]);
-      const jsonString = final.map((e) => JSON.stringify(e)).join(",\n");
-      console.log(intersection.length);
-      await fs.writeFileSync("duplicates.json", jsonString);
-    })
-    .on("error", (error) => {
-      console.log(`Error reading CSV file: ${error}`);
+      console.log(vendors);
+      // vendors.forEach(s=>{
+      //   console.log(s.split(" | ")[1],"p")
+      // })
+      const intersection = vendors.filter((item) => !daily.has(item.split(" | ")[1]));
+      console.log("Duplicates count:", intersection.length);
+      // console.log(intersection,"intermpc")
+      let final = intersection.map((e) => e);
+      let jsonString = final.map((e) => JSON.stringify(e)).join("\n");
+      jsonString = jsonString.replace(/" "/g, "");
+
+      await fs.writeFileSync("duplicates.csv", jsonString);
     });
+  //   const bigdata = [];
+  //   const daily = [];
+  //   fs.createReadStream("./CSV/newFile.csv")
+  //     .pipe(parse())
+  //     .on("data", (row) => {
+  //       row[0] !== "" && daily.push(row[0]);
+  //       row[1] !== "" && bigdata.push(row[1]);
+  //     })
+  //     .on("end", async () => {
+  //       console.log(daily.length, bigdata.length);
+  //       console.log("Inprogress");
+
+  //       let intersection = bigdata.filter((x) => !daily.includes(x));
+  //       // let final = intersection.map((e) => [e]);
+  //       // const jsonString = final.map((e) => JSON.stringify(e)).join(",\n");
+  //       console.log(intersection.length);
+  //       console.log("Completed");
+
+  //       await fs.writeFileSync("duplicates.json", jsonString);
+  //     })
+  //     .on("error", (error) => {
+  //       console.log(`Error reading CSV file: ${error}`);
+  //     });
 };
 
 bigdataFile();
