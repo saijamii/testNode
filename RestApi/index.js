974782e1bfadf903a72b8d130a2eef338c136b80
saijamii <saijami38@gmail.com>
2024-07-09 09:21:32 +0530

    Methods

--- RestApi/index.js
+++ RestApi/index.js
@@ -1,11 +1,80 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.all("/", (req, res) => {
-  console.log("Request > ", req);
-  console.log("Response > ", res);
+  // console.log("Request > ", req);
+  // console.log("Response > ", res);
   res.send("Im Response!");
+});
+
+const Todos = [
+  {
+    id: 1,
+    title: "Task 1 ",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Task 2 ",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Task 3 ",
+    completed: true,
+  },
+];
+
+//Read
+
+app.get("/todos", (req, res) => {
+  res.json(Todos);
+});
+
+//Create
+
+app.post("/todos", (req, res) => {
+  const newTodo = req.body;
+  Todos.push(newTodo);
+  res.json({
+    message: "New Todo Added",
+  });
+});
+
+//Update
+
+app.put("/todos/:id", (req, res) => {
+  const newTodoData = req.body;
+  const todoParamId = Number(req.params.id);
+  const todoIndex = Todos.findIndex((e) => e.id === todoParamId);
+  if (todoIndex !== -1) {
+    Todos[todoIndex] = {
+      id: todoParamId,
+      ...newTodoData,
+    };
+  }
+
+  res.json({
+    message: "Todo updated successfully",
+  });
+});
+
+//Delete
+
+app.delete("/todos/:id", (req, res) => {
+  const todoParamId = Number(req.params.id);
+  const todoIndex = Todos.findIndex((e) => e.id === todoParamId);
+  if (todoIndex !== -1) {
+    Todos.splice(todoIndex, 1);
+  }
+
+  res.json({
+    message: "Todo deleted successfully",
+  });
 });
 
 const PORT = 8000;
