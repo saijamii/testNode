c45d224dcf728a95026d014ca2d51dbe65000097
saijamii <saijami38@gmail.com>
2024-07-26 09:35:07 +0530

    nested Data

--- Graphql/resolvers.js
+++ Graphql/resolvers.js
@@ -1,21 +1,33 @@
+const data = {
+  authors: [
+    { id: "1", name: "Author 1", bookIds: ["101", "102"] },
+    { id: "2", name: "Author 2", bookIds: ["103", "104"] },
+    { id: "3", name: "Author 3", bookIds: ["104", "105"] },
+  ],
+  books: [
+    { id: "101", title: "Book 1", publishedYear: 2000, authorld: "1" },
+    { id: "102", title: "Book 2", publishedYear: 2000, authorld: "1" },
+    { id: "103", title: "Book 3", publishedYear: 2000, authorld: "2" },
+    { id: "105", title: "Book 3", publishedYear: 2000, authorld: "3" },
+    { id: "104", title: "Book 3", publishedYear: 2000, authorld: "3" },
+  ],
+};
+
 export const resolvers = {
-  Query: {
-    authors: () => {
-      return [
-        {
-          id: 1,
-          name: "S1",
-        },
-      ];
+  Book: {
+    author: (parent, args, context, info) => {
+      console.log(parent);
+      return data.authors.find(
+        (authorDetail) => authorDetail.id === parent.authorld
+      );
     },
-    books: () => {
-      return [
-        {
-          id: 1,
-          title: "Book1",
-          publishedYear: 221,
-        },
-      ];
+  },
+  Query: {
+    authors: (parent, args, context, info) => {
+      return data.authors;
+    },
+    books: (parent, args, context, info) => {
+      return data.books;
     },
   },
 };
--- Graphql/typeDefs.js
+++ Graphql/typeDefs.js
@@ -3,12 +3,14 @@ export const typeDefs = `#graphql
 type Author {
     id : ID! 
     name : String!
+    books : [Book]
 }
 
 type Book {
     id : ID! 
     title : String!
     publishedYear : Int
+    author : Author
 }
 
 type Query {
